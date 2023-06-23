e113d8bda30302a36800a6888be4b0c3c1293d59
Sabrina Coelho <80803237+SabrinaCoelho@users.noreply.github.com>
2023-06-23 13:59:54 -0300

    Update cameras dos rovers

--- src/cameras.js
+++ src/cameras.js
@@ -8,23 +8,131 @@ const camerasData = {
             nome: "Rover Down-Look Camera",
             abbreviation: "EDL_RDCAM"
         },
+        {
+            nome: "Descent Stage Down-Look Camera",
+            abbreviation: "EDL_DDCAM"
+        },
+        {
+            nome: "Parachute Up-Look Camera A",
+            abbreviation: "EDL_PUCAM1"
+        },
+        {
+            nome: "Parachute Up-Look Camera B",
+            abbreviation: "EDL_PUCAM2"
+        },
+        {
+            nome: "Navigation Camera - Left",
+            abbreviation: "NAVCAM_LEFT"
+        },
+        {
+            nome: "Navigation Camera - Right",
+            abbreviation: "NAVCAM_RIGHT"
+        },
+        {
+            nome: "Mast Camera Zoom - Right",
+            abbreviation: "MCZ_RIGH"
+        },
+        {
+            nome: "Mast Camera Zoom - Left",
+            abbreviation: "MCZ_LEFT"
+        },
+        {
+            nome: "Front Hazard Avoidance Camera - Left",
+            abbreviation: "FRONT_HAZCAM_LEFT_A"
+        },
+        {
+            nome: "Front Hazard Avoidance Camera - Right",
+            abbreviation: "FRONT_HAZCAM_RIGHT_A"
+        },
+        {
+            nome: "Rear Hazard Avoidance Camera - Left",
+            abbreviation: "REAR_HAZCAM_LEFT"
+        },
+        {
+            nome: "Rear Hazard Avoidance Camera - Right",
+            abbreviation: "REAR_HAZCAM_RIGHT"
+        },
+        {
+            nome: "MEDA Skycam",
+            abbreviation: "SKYCAM"
+        },
+        {
+            nome: "SHERLOC WATSON Camera",
+            abbreviation: "SHERLOC_WATSON"
+        },
     ],
     curiosity: [
         {
-            nome: "curiosity",
-            abbreviation: "curiosity"
+            nome: "Front Hazard Avoidance Camera",
+            abbreviation: "FHAZ"
+        },
+        {
+            nome: "Rear Hazard Avoidance Camera",
+            abbreviation: "RHAZ"
+        },
+        {
+            nome: "Mast Camera",
+            abbreviation: "MAST"
+        },
+        {
+            nome: "Chemistry and Camera Complex",
+            abbreviation: "CHEMCAM"
+        },
+        {
+            nome: "Mars Hand Lens Imager",
+            abbreviation: "MAHLI"
+        },
+        {
+            nome: "Mars Descent Imager",
+            abbreviation: "MARDI"
+        },
+        {
+            nome: "Navigation Camera",
+            abbreviation: "NAVCAM"
         },
     ],
     opportunity: [
         {
-            nome: "opportunity",
-            abbreviation: "opportunity"
+            nome: "Front Hazard Avoidance Camera",
+            abbreviation: "FHAZ"
+        },
+        {
+            nome: "Rear Hazard Avoidance Camera",
+            abbreviation: "RHAZ"
+        },
+        {
+            nome: "Navigation Camera",
+            abbreviation: "NAVCAM"
+        },
+        {
+            nome: "Panoramic Camera",
+            abbreviation: "PANCAM"
+        },
+        {
+            nome: "Miniature Thermal Emission Spectrometer (Mini-TES)",
+            abbreviation: "MINITES"
         },
     ],
     spirit: [
         {
-            nome: "spirit",
-            abbreviation: "spirit"
+            nome: "Front Hazard Avoidance Camera",
+            abbreviation: "FHAZ"
+        },
+        {
+            nome: "Rear Hazard Avoidance Camera",
+            abbreviation: "RHAZ"
+        },
+        {
+            nome: "Navigation Camera",
+            abbreviation: "NAVCAM"
+        },
+        {
+            nome: "Panoramic Camera",
+            abbreviation: "PANCAM"
+        },
+        {
+            nome: "Miniature Thermal Emission Spectrometer (Mini-TES)",
+            abbreviation: "MINITES"
         },
     ]
 }
